f215510dc596572e87441b7439819f03b364ddf6
Pinkovay <pedros.pinkovay@gmail.com>
2022-08-10 15:51:38 -0300

    Create desafio32.py

---
+++ desafio32.py
@@ -0,0 +1,9 @@
+import datetime
+ano = int(input('Qual ano deseja analisar? Coloque (00) para analisar o ano atual: '))
+if ano == 00:
+   ano = datetime.date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano}, é BISSEXTO')
+else:
+    print(f'O ano {ano}, não é BISSEXTO')
+    
