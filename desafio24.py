5fcea6e1c0f6c42cb06bfab8d84a87abddbed80a
Pinkovay <pedros.pinkovay@gmail.com>
2022-08-10 15:42:11 -0300

    Create desafio24.py

---
+++ desafio24.py
@@ -0,0 +1,8 @@
+cidade = str(input('Qual cidade você nasceu? ')).strip()
+print(cidade[:5].upper() == 'SANTO')
+#dividir0 = cidade.upper()
+#dividir = dividir0.split()
+#dividir1 = dividir[0]
+#dividir01 = dividir1.lstrip() , dividir1.rstrip()
+#dividir2 = 'SANTO' in dividir1
+#print(dividir2)
