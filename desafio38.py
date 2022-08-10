e6685f713a32f28a42c854215c3d523c61e8971b
Pinkovay <pedros.pinkovay@gmail.com>
2022-08-10 15:58:55 -0300

    Create desafio38.py

---
+++ desafio38.py
@@ -0,0 +1,8 @@
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+if n1 > n2:
+    print(F'O número {n1} é o maior.')
+elif n2 > n1:
+    print(F'O número {n2} é o maior.')
+elif n1 == n2:
+    print(F'Os dois números são iguais.')
