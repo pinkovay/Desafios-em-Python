0223752930bcd03593b246cae2385e31d7a4372a
Pinkovay <pedros.pinkovay@gmail.com>
2022-08-10 15:47:20 -0300

    Update and rename desafio26 to desafio26.py

--- desafio26.py
+++ desafio26.py
@@ -3,3 +3,4 @@ a = frase.count('A')
 print(f'A letra (A) aparece {a} vezes nesta frase/palavra/nome. ')
 print('E a primeira vez que ela aparece é na casa:', frase.find('A')+1)
 print('A ultima vez é na casa:', frase.rfind('A')+1)
+
